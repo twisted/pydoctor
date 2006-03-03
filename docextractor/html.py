7ddcb5f5bea53ae8c83f20b595531add5fd5bc11
mwh <>
2006-03-03 13:33:24 +0000

    report interfaces implemented indirectly as well.

--- docextractor/html.py
+++ docextractor/html.py
@@ -248,17 +248,28 @@ class SystemWriter(object):
         if cls.subclasses:
             x += '<p>known subclasses: %s</p>'%(', '.join(
                 map(linkto, cls.subclasses)))
-        if cls.isinterface and cls.implementedby:
-            x += '<p>known implementations: %s</p>'%(', '.join(
-                map(linkto, cls.implementedby)))
-        elif cls.implements:
+        if cls.isinterface and (cls.implementedby_directly or cls.implementedby_indirectly):
             links = []
-            for interface in cls.implements:
-                if interface in cls.system.allobjects:
-                    links.append(linkto(cls.system.allobjects[interface]))
-                else:
-                    links.append(interface)
-            x += '<p>implements interfaces: %s</p>'%(', '.join(links))
+            for template, implementedby in [('<i>%s</i>', cls.implementedby_indirectly),
+                                         ('%s', cls.implementedby_directly)]:
+                for implementation in implementedby:
+                    if implementation in cls.system.allobjects:
+                        l = linkto(cls.system.allobjects[implementation])
+                    else:
+                        l = implementation
+                    links.append(template%(l,))
+            x += '<p>known implementations: %s</p>'%(', '.join(links),)
+        elif cls.implements_directly or cls.implements_indirectly:
+            links = []
+            for template, implements in [('%s', cls.implements_directly),
+                                         ('<i>%s</i>', cls.implements_indirectly)]:
+                for interface in implements:
+                    if interface in cls.system.allobjects:
+                        l = linkto(cls.system.allobjects[interface])
+                    else:
+                        l = interface
+                    links.append(template%(l,))
+            x += '<p>implements interfaces: %s</p>'%(', '.join(links),)
         z = doc2html(cls, cls.docstring)
         x += '<div class="toplevel">%s</div>' % (z,)
         link = lambda x: '#' + x.fullName()
@@ -278,7 +289,7 @@ class SystemWriter(object):
         return x
 
     def interfaceInfo(self, cls, meth):
-        for interface in cls.implements:
+        for interface in cls.implements_directly + cls.implements_indirectly:
             if interface in cls.system.allobjects:
                 io = cls.system.allobjects[interface]
                 if meth.name in io.contents:
--- docextractor/twistedmodel.py
+++ docextractor/twistedmodel.py
@@ -4,16 +4,19 @@ from docextractor import ast_pp
 class TwistedClass(model.Class):
     isinterface = False
     implementsOnly = False
+    implementedby_directly = None # [objects], when isinterface == True
+    implementedby_indirectly = None # [objects], when isinterface == True
     def setup(self):
         super(TwistedClass, self).setup()
-        self.implements = [] # [name of interface]
-        self.allimplements = [] # [(interface name, directly implemented)]
-        self.implementedby = [] # [objects]
+        self.implements_directly = [] # [name of interface]
+        self.implements_indirectly = [] # [(interface name, directly implemented)]
 
 def addInterfaceInfoToClass(cls, interfaceargs, implementsOnly):
     cls.implementsOnly = implementsOnly
+    if implementsOnly:
+        cls.implements_directly = []
     for arg in interfaceargs:
-        cls.implements.append(
+        cls.implements_directly.append(
             cls.dottedNameToFullName(ast_pp.pp(arg)))
     
 
@@ -49,21 +52,56 @@ class TwistedSystem(model.System):
             self.push(self.allobjects[tpc])
             self.pushClass('Interface', None)
             self.popClass()
+            self.pop(self.allobjects[tpc])
         super(TwistedSystem, self).finalStateComputations()
+
         if tpc in self.allobjects:
             self.markInterface(self.allobjects[tpc+'.Interface'])
         for cls in self.objectsOfType(model.Class):
             if 'zope.interface.Interface' in cls.bases:
                 self.markInterface(cls)
+
         for cls in self.objectsOfType(model.Class):
-            for interface in cls.implements:
+            if cls.isinterface or len(cls.baseobjects) != cls.baseobjects.count(None):
+                continue
+            self.push_implements_info(cls, cls.implements_directly)
+
+        for cls in self.objectsOfType(model.Class):
+            for interface in cls.implements_directly:
                 if interface in self.allobjects and '.test.' not in interface:
-                    self.allobjects[interface].implementedby.append(cls)
+                    interface_ob = self.allobjects[interface]
+                    if interface_ob.implementedby_directly is None:
+                        self.warning("probable interface not marked as such",
+                                     interface_ob.fullName())
+                        interface_ob.implementedby_directly = []
+                        interface_ob.implementedby_indirectly = []
+                    interface_ob.implementedby_directly.append(cls.fullName())
+            for interface in cls.implements_indirectly:            
+                if interface in self.allobjects and '.test.' not in interface:
+                    interface_ob = self.allobjects[interface]
+                    if interface_ob.implementedby_indirectly is None:
+                        self.warning("probable interface not marked as such",
+                                     interface_ob.fullName())
+                        interface_ob.implementedby_directly = []
+                        interface_ob.implementedby_indirectly = []
+                    interface_ob.implementedby_indirectly.append(cls.fullName())
+        
 
     def markInterface(self, cls):
         cls.isinterface = True
         cls.kind = "Interface"
+        cls.implementedby_directly = []
+        cls.implementedby_indirectly = []
         for sc in cls.subclasses:
             if '.test.' not in sc.fullName():
                 self.markInterface(sc)
 
+    def push_implements_info(self, cls, interfaces):
+        for ob in cls.subclasses:
+            ob.implements_indirectly = interfaces[:]
+            if ob.implementsOnly:
+                ob.implements_indirectly = []
+                newinterfaces = ob.implements_directly
+            else:
+                newinterfaces = interfaces + ob.implements_directly
+            self.push_implements_info(ob, newinterfaces)
