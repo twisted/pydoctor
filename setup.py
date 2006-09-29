12542a4428b126769c602b493833242f4700153c
mwh <>
2006-09-29 08:42:47 +0000

    add a license, update version in setup.py to 0.2 (time for another stealth release, somebody wants to package pydoctor for debian...)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pydoctor',
-    version='0.1',
+    version='0.2',
     author='Michael Hudson',
     author_email='mwh@python.net',
     url='http://codespeak.net/svn/user/mwh/pydoctor/trunk',
