bb616eb221ccf5339fca25895eea4e225dec2256
mwh <>
2007-01-07 14:10:17 +0000

    give slightly smarter progress information during html generation

--- pydoctor/nevowhtml/writer.py
+++ pydoctor/nevowhtml/writer.py
@@ -9,6 +9,8 @@ class NevowWriter:
     def __init__(self, filebase):
         self.base = filebase
         self.written_pages = 0
+        self.total_pages = 0
+        self.dry_run = False
 
     def prepOutputDirectory(self):
         if not os.path.exists(self.base):
@@ -23,16 +25,25 @@ class NevowWriter:
                             os.path.join(self.base, 'pydoctor.js'))
 
     def writeIndividualFiles(self, obs, functionpages=False):
+        self.dry_run = True
+        for ob in obs:
+            self.writeDocsFor(ob, functionpages=functionpages)
+        self.dry_run = False
         for ob in obs:
             self.writeDocsFor(ob, functionpages=functionpages)
         print
 
     def writeModuleIndex(self, system):
-        for pclass in summary.summarypages:
+        import time
+        for i, pclass in enumerate(summary.summarypages):
+            print 'starting', pclass.__name__, '...',
+            sys.stdout.flush()
+            T = time.time()
             page = pclass(system)
             f = open(os.path.join(self.base, pclass.filename), 'w')
             f.write(flat.flatten(page))
             f.close()
+            print "took", time.time() - T, 's'
 
     def writeDocsFor(self, ob, functionpages):
         isfunc = ob.document_in_parent_page
@@ -44,6 +55,9 @@ class NevowWriter:
             self.writeDocsFor(o, functionpages)
 
     def writeDocsForOne(self, ob, fobj):
+        if self.dry_run:
+            self.total_pages += 1
+            return
         # brrrrrrrr!
         d = pages.__dict__
         for c in ob.__class__.__mro__:
@@ -58,6 +72,6 @@ class NevowWriter:
         page = pclass(ob)
         self.written_pages += 1
         if self.system.options.verbosity == 0:
-            print '\rwritten', self.written_pages, 'pages',
+            print '\rwritten', self.written_pages, '/', self.total_pages, 'pages',
         sys.stdout.flush()
         fobj.write(flat.flatten(page))
