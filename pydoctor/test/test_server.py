5c0eb8349cc510866672b3c2b775f62813d25257
mwh <>
2007-04-21 14:03:50 +0000

    porting patches from bzr... ------------------------------------------------------------ revno: 337 committer: Michael Hudson <mwh@quickbeam.local> branch nick: pydoctor.dev timestamp: Sat 2007-04-21 11:50:05 +0200 message:   test editing the docstring

--- pydoctor/test/test_server.py
+++ pydoctor/test/test_server.py
@@ -9,8 +9,13 @@ def deferredResult(*args):
     maybeDeferred(*args).addCallback(lambda x:hack.append(x))
     return hack[0]
 
-def getTextOfPage(root, page, args=None):
+def getTextOfPage(root, page, args=None, return_request=False):
     """This perpetrates several awful hacks."""
+    if args is not None:
+        args_ = {}
+        for k, v in args.iteritems():
+            args_[k] = [v]
+        args = args_
     r = testutil.FakeRequest(args=args)
     r.postpath = [page]
     ctx = context.RequestContext(tag=r)
@@ -18,7 +23,10 @@ def getTextOfPage(root, page, args=None):
     while 1:
         html = deferredResult(pageContext.tag.renderHTTP, pageContext)
         if isinstance(html, str):
-            return r.v + html
+            if return_request:
+                return r.v + html, r
+            else:
+                return r.v + html
         res = inevow.IResource(html)
         pageContext = context.PageContext(tag=res, parent=pageContext)
 
@@ -34,11 +42,28 @@ def test_simple():
 def test_edit_renders_ok():
     system = processPackage('localimporttest')
     root = server.EditingPyDoctorResource(system)
-    args = {'ob':['localimporttest.mod1.C']}
+    args = {'ob':'localimporttest.mod1.C'}
     result = getTextOfPage(root, 'edit', args=args)
     # very weak, but it's an assert that things didn't explode
     assert 'textarea' in result
 
-    args = {'ob':['does.not.exist']}
+    args = {'ob':'does.not.exist'}
     result = getTextOfPage(root, 'edit', args=args)
     assert 'An error occurred' in result
+
+def test_edit():
+    system = processPackage('localimporttest')
+    root = server.EditingPyDoctorResource(system)
+
+    ob = system.allobjects['localimporttest.mod1.C']
+    docstring = root.currentDocstringForObject(ob)
+    assert docstring == ob.docstring
+
+    args = {'ob':ob.fullName(), 'docstring':'"This *is* a docstring"',
+            'action':'Submit'}
+    result, r = getTextOfPage(root, 'edit', args=args, return_request=True)
+    assert not result
+    assert 'localimporttest.mod1.C' in r.redirected_to
+    docstring = root.currentDocstringForObject(ob)
+    assert docstring == eval(args['docstring'])
+    assert ob.docstring != docstring
