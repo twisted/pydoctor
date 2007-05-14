dd07a3f987a6211e4a9d431f9554772da1622279
mwh <>
2007-05-14 06:55:11 +0000

    porting patches from bzr... ------------------------------------------------------------ revno: 384 committer: Michael Hudson <mwh@mithril.local> branch nick: pydoctor.dev timestamp: Sun 2007-05-13 17:51:53 +0100 message:   i don't think that was meant to be there

--- pydoctor/zopeinterface.py
+++ pydoctor/zopeinterface.py
@@ -120,27 +120,6 @@ class ZopeInterfaceModuleVisitor(astbuilder.ModuleVistor):
                                 funcName == 'zope.interface.classImplementsOnly')
     visitCallFunc_zope_interface_classImplementsOnly = visitCallFunc_zope_interface_classImplements
 
-    def visitCallFunc_twisted_python_util_moduleMovedForSplit(self, funcName, node):
-        # XXX this is rather fragile...
-        origModuleName, newModuleName, moduleDesc, \
-                        projectName, projectURL, globDict = node.args
-        moduleDesc = ast_pp.pp(moduleDesc)[1:-1]
-        projectName = ast_pp.pp(projectName)[1:-1]
-        projectURL = ast_pp.pp(projectURL)[1:-1]
-        modoc = """
-%(moduleDesc)s
-
-This module is DEPRECATED. It has been split off into a third party
-package, Twisted %(projectName)s. Please see %(projectURL)s.
-
-This is just a place-holder that imports from the third-party %(projectName)s
-package for backwards compatibility. To use it, you need to install
-that package.
-""" % {'moduleDesc': moduleDesc,
-       'projectName': projectName,
-       'projectURL': projectURL}
-        self.builder.current.docstring = modoc
-
 
 def markInterfaceClass(cls):
     cls.isinterfaceclass = True
