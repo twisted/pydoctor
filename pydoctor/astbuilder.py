e2c774b86395b7b9ba3d969c383d2a2abbe95ba6
Michael Hudson <michael.hudson@canonical.com>
2007-09-11 00:09:46 +0100

    found a bug

--- pydoctor/astbuilder.py
+++ pydoctor/astbuilder.py
@@ -127,6 +127,7 @@ class ModuleVistor(object):
 
     def visitFrom(self, node):
         modname = self.builder.expandModname(node.modname)
+        # XXX what about "from package import mod1, mod2" ??
         mod = self.system.getProcessedModule(modname)
         if mod is not None:
             assert mod.state in [model.PROCESSING, model.PROCESSED]
