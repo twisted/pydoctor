62b30014a7f7da4153a52145e40b10cf9136049e
Michael Hudson <michael.hudson@canonical.com>
2010-02-06 15:39:05 +1300

    doc updates

--- www/conftest.py
+++ www/conftest.py
@@ -1,7 +1,8 @@
 import py
-import re
+
 
 class TidyChecker(py.test.collect.Item):
+
     def run(self):
         tidy = py.path.local.sysfind('tidy')
         if not tidy:
@@ -10,15 +11,19 @@ class TidyChecker(py.test.collect.Item):
 
 
 class Module(py.test.collect.Module):
+
     def run(self):
         #html = self.fspath.read()
         # find links, somehow...
         return ['tidy']
+
     def join(self, name):
         if name == 'tidy':
             return TidyChecker(self.name, parent=self)
 
+
 class Directory(py.test.collect.Directory):
+
     def run(self):
         results = super(Directory, self).run()
         for x in self.fspath.listdir('*.html', sort=True):
@@ -26,9 +31,8 @@ class Directory(py.test.collect.Directory):
         return results
 
     def join(self, name):
-        if not name.endswith('.html'): 
-            return super(DocDirectory, self).join(name) 
+        if not name.endswith('.html'):
+            return super(Directory, self).join(name)
         p = self.fspath.join(name)
-        if p.check(file=1): 
-            return Module(p, parent=self) 
-        
+        if p.check(file=1):
+            return Module(p, parent=self)
