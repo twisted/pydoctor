4f1388ef07728b72bee9657a84114b2791dc990c
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-11-15 12:33:37 +1300

    setup.py updates

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 
 setup(
     name='pydoctor',
-    version='0.3',
-    author='Michael Hudson',
+    version='0.4a1',
+    author='Michael Hudson-Doyle',
     author_email='micahel@gmail.com',
-    url='http://codespeak.net/svn/user/mwh/pydoctor/trunk',
+    url='http://codespeak.net/~mwh/pydoctor/',
     description='API doc generator.',
     license='MIT/X11',
     packages=[
