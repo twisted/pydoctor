79aa2bba1e1cd8d6a6425dd4b3db6fabb27166b0
HawkOwl <hawkowl@atleastfornow.net>
2015-08-09 10:04:35 +0800

    add trove classifiers

--- setup.py
+++ setup.py
@@ -13,14 +13,25 @@ setup(
         'pydoctor',
         'pydoctor.templatewriter',
         'pydoctor.templatewriter.pages',
-        ],
+    ],
     package_data={
         'pydoctor': [
             'templates/*',
-            ],
-        },
+        ],
+    },
     scripts=[
         'bin/pydoctor',
-        ],
+    ],
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Documentation',
+        'Topic :: Software Development :: Documentation',
+    ],
     install_requires=["Twisted", "epydoc"],
-    )
+)
