b02e5ce2c1dc45cfa5ae430c429f66f1a21880f0
David Reid <dreid@dreid.org>
2016-08-08 09:34:53 -0700

    scope.

--- pydoctor/templates/pydoctor.js
+++ pydoctor/templates/pydoctor.js
@@ -2,7 +2,7 @@ function togglePrivate() {
     // Hide all private things by adding the private-hidden class to them.
     document.body.classList.toggle("private-hidden");
 
-    button = document.querySelector('#showPrivate button');
+    var button = document.querySelector('#showPrivate button');
     if(document.body.classList.contains('private-hidden')) {
         button.innerText = 'Show Private API';
     } else {
