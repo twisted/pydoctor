49908660afdb3f9c8cdea5a70d01dea3235b4c24
Maarten ter Huurne <maarten@treewalker.org>
2019-07-28 11:19:09 +0200

    Import of epydoc, SVN revision 1812

---
+++ pydoctor/epydoc/docwriter/__init__.py
@@ -0,0 +1,12 @@
+# epydoc -- Output generation
+#
+# Copyright (C) 2005 Edward Loper
+# Author: Edward Loper <edloper@loper.org>
+# URL: <http://epydoc.sf.net>
+#
+# $Id$
+
+"""
+Output generation.
+"""
+__docformat__ = 'epytext en'
