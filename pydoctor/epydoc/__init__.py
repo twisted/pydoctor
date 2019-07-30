c60fdf40f1c0478381bb7f29560b753dcd0e2449
Maarten ter Huurne <maarten@treewalker.org>
2019-07-30 04:27:34 +0200

    Remove epydoc parser and introspection modules
    pydoctor has its own parser.

--- pydoctor/epydoc/__init__.py
+++ pydoctor/epydoc/__init__.py
@@ -7,138 +7,9 @@
 # $Id$
 
 """
-Automatic Python reference documentation generator.  Epydoc processes
-Python modules and docstrings to generate formatted API documentation,
-in the form of HTML pages.  Epydoc produces API documentation using
-the following steps:
 
-1. Extract basic information about the specified objects, and objects
-   that are related to them (such as the values defined by a module).
-   This can be done via introspection, parsing, or both:
-
-   * *Introspection* imports the objects, and examines them directly
-     using Python's introspection mechanisms.
-
-   * *Parsing* reads the Python source files that define the objects,
-     and extracts information from those files.
-
-2. Combine and process that information.
-
-   * **Merging**: Merge the information obtained from introspection &
-     parsing each object into a single structure.
-
-   * **Linking**: Replace any \"pointers\" that were created for
-     imported variables with the documentation that they point to.
-
-   * **Naming**: Assign unique *canonical names* to each of the
-     specified objects, and any related objects.
-
-   * **Docstrings**: Parse the docstrings of each of the specified
-     objects.
-
-   * **Inheritance**: Add variables to classes for any values that
-     they inherit from their base classes.
-
-3. Generate output.  Output can be generated in a variety of formats:
-
-   * An HTML webpage.
-
-   * A LaTeX document (which can be rendered as a PDF file)
-
-   * A plaintext description.
-
-.. digraph:: Overview of epydoc's architecture
-   :caption: The boxes represent steps in epydoc's processing chain.
-             Arrows are annotated with the data classes used to
-             communicate between steps.  The lines along the right
-             side mark what portions of the processing chain are
-             initiated by build_doc_index() and cli().  Click on
-             any item to see its documentation.
-
-   /*
-                  Python module or value                 *       *
-                      /           \                      |       |
-                     V             V                     |       |
-            introspect_docs()  parse_docs()              |       |
-                        \        /                       |       |
-                         V      V                        |       |
-                        merge_docs()                     |       |
-                             |              build_doc_index()  cli()
-                             V                           |       |
-                       link_imports()                    |       |
-                             |                           |       |
-                             V                           |       |
-                    assign_canonical_names()             |       |
-                             |                           |       |
-                             V                           |       |
-                      parse_docstrings()                 |       |
-                             |                           |       |
-                             V                           |       |
-                       inherit_docs()                    *       |
-                      /      |        \                          |
-                     V       V         V                         |
-                HTMLWriter LaTeXWriter PlaintextWriter           *
-   */
-
-   ranksep = 0.1;
-   node [shape="box", height="0", width="0"]
-
-   { /* Task nodes */
-     node [fontcolor=\"#000060\"]
-     introspect  [label="Introspect value:\\nintrospect_docs()",
-                  href="<docintrospecter.introspect_docs>"]
-     parse       [label="Parse source code:\\nparse_docs()",
-                  href="<docparser.parse_docs>"]
-     merge       [label="Merge introspected & parsed docs:\\nmerge_docs()",
-                  href="<docbuilder.merge_docs>", width="2.5"]
-     link        [label="Link imports:\\nlink_imports()",
-                  href="<docbuilder.link_imports>", width="2.5"]
-     name        [label="Assign names:\\nassign_canonical_names()",
-                  href="<docbuilder.assign_canonical_names>", width="2.5"]
-     docstrings  [label="Parse docstrings:\\nparse_docstring()",
-                  href="<docstringparser.parse_docstring>", width="2.5"]
-     inheritance [label="Inherit docs from bases:\\ninherit_docs()",
-                  href="<docbuilder.inherit_docs>", width="2.5"]
-     write_html  [label="Write HTML output:\\nHTMLWriter",
-                 href="<docwriter.html>"]
-     write_latex  [label="Write LaTeX output:\\nLaTeXWriter",
-                 href="<docwriter.latex>"]
-     write_text  [label="Write text output:\\nPlaintextWriter",
-                 href="<docwriter.plaintext>"]
-   }
-
-   { /* Input & Output nodes */
-     node [fontcolor=\"#602000\", shape="plaintext"]
-     input [label="Python module or value"]
-     output [label="DocIndex", href="<apidoc.DocIndex>"]
-   }
-
-   { /* Graph edges */
-     edge [fontcolor=\"#602000\"]
-     input -> introspect
-     introspect -> merge [label="APIDoc", href="<apidoc.APIDoc>"]
-     input -> parse
-     parse -> merge [label="APIDoc", href="<apidoc.APIDoc>"]
-     merge -> link [label=" DocIndex", href="<apidoc.DocIndex>"]
-     link -> name [label=" DocIndex", href="<apidoc.DocIndex>"]
-     name -> docstrings [label=" DocIndex", href="<apidoc.DocIndex>"]
-     docstrings -> inheritance [label=" DocIndex", href="<apidoc.DocIndex>"]
-     inheritance -> output
-     output -> write_html
-     output -> write_latex
-     output -> write_text
-   }
-
-   { /* Task collections */
-     node [shape="circle",label="",width=.1,height=.1]
-     edge [fontcolor="black", dir="none", fontcolor=\"#000060\"]
-     l3 -> l4 [label=" epydoc.\\l docbuilder.\\l build_doc_index()",
-               href="<docbuilder.build_doc_index>"]
-     l1 -> l2 [label=" epydoc.\\l cli()", href="<cli>"]
-   }
-   { rank=same; l1 l3 input }
-   { rank=same; l2 write_html }
-   { rank=same; l4 output }
+epydoc is an automatic Python reference documentation generator.
+pydoctor uses parts of the epydoc source as a library.
 
 Package Organization
 ====================
@@ -149,15 +20,13 @@ The epydoc package contains the following subpackages and modules:
 
 The user interfaces are provided by the `gui` and `cli` modules.
 The `apidoc` module defines the basic data types used to record
-information about Python objects.  The programmatic interface to
-epydoc is provided by `docbuilder`.  Docstring markup parsing is
+information about Python objects.  Docstring markup parsing is
 handled by the `markup` package, and output generation is handled by
 the `docwriter` package.  See the submodule list for more
 information about the submodules and subpackages.
 
 :group Basic Data Types: apidoc
-:group Documentation Generation: docbuilder, docintrospecter, docparser
-:group Docstring Processing: docstringparser, markup
+:group Docstring Processing: markup
 :group Output Generation: docwriter
 :group Miscellaneous: log, util, test, compat
 
