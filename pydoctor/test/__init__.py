41de6338c7c839899f151555c22bec8e050228e4
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-07-01 17:10:45 +0200

    Make unit tests pass on Python 3.5
    Some of our unit tests use syntax introduced in Python 3.6; we can
    filter those out by making the @py3only annotation actually check
    for 3.6+.
    
    I don't plan to support Pyton 3.5 long term, but it is a small effort
    to include it in this release.

--- pydoctor/test/__init__.py
+++ pydoctor/test/__init__.py
@@ -4,6 +4,6 @@ from __future__ import print_function
 import sys
 import pytest
 
-py3only = pytest.mark.skipif(sys.version_info < (3, 0), reason="requires python 3")
+py3only = pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python 3")
 py2only = pytest.mark.skipif(sys.version_info >= (3, 0), reason="requires python 2")
 typecomment = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8")
