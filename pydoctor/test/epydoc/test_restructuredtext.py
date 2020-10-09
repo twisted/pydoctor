b53c6f0095343eb95744a5204de14de396402134
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-10-09 11:21:58 +0200

    Fix handling of external links in restructured text
    For external links, an extra 'target' attribute gets added, which
    modifies the dictionary being iterated over.
    
    This likely got broken in the porting to Python 3, since in Python 2
    dict.items() made a copy while it returns an items view in Python 3.

--- pydoctor/test/epydoc/test_restructuredtext.py
+++ pydoctor/test/epydoc/test_restructuredtext.py
@@ -1,5 +1,12 @@
+from pydoctor.epydoc.markup import flatten
 from pydoctor.epydoc.markup.restructuredtext import parse_docstring
 
+
+def rst2html(s):
+    errors = []
+    parsed = parse_docstring(s, errors)
+    assert not errors
+    return flatten(parsed.to_stan(None))
 
 def test_rst_anon_link_target_missing():
     src = """
@@ -10,3 +17,10 @@ def test_rst_anon_link_target_missing():
     assert len(errors) == 1
     assert errors[0].descr().startswith("Anonymous hyperlink mismatch:")
     assert errors[0].is_fatal()
+
+def test_rst_anon_link_email():
+    src = "`<postmaster@example.net>`__"
+    html = rst2html(src)
+    assert html.startswith('<a ')
+    assert ' href="mailto:postmaster@example.net"' in html
+    assert html.endswith('>mailto:postmaster@example.net</a>')
