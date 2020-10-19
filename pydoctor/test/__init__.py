0495addd5883bf6532971b928be328c1b1c98deb
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-10-19 21:11:03 +0200

    Use Protocol for pytest fixture type approximations
    This is less likely to lead to conflicts when pytest publishes
    official type annotations for textures.

--- pydoctor/test/__init__.py
+++ pydoctor/test/__init__.py
@@ -1,7 +1,7 @@
 """PyDoctor's test suite."""
 
 from logging import LogRecord
-from typing import NamedTuple, Sequence
+from typing import TYPE_CHECKING, Sequence
 import sys
 import pytest
 
@@ -11,12 +11,17 @@ typecomment = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires pyt
 # Because pytest 6.1 does not yet export types for fixtures, we define
 # approximations that are good enough for our test cases:
 
-class CapLog:
-    records: Sequence[LogRecord]
+if TYPE_CHECKING:
+    from typing_extensions import Protocol
 
-class CaptureResult(NamedTuple):
-    out: str
-    err: str
+    class CapLog(Protocol):
+        records: Sequence[LogRecord]
 
-class CapSys:
-    def readouterr(self) -> CaptureResult: ...
+    class CaptureResult(Protocol):
+        out: str
+        err: str
+
+    class CapSys(Protocol):
+        def readouterr(self) -> CaptureResult: ...
+else:
+    CapLog = CaptureResult = CapSys = object
