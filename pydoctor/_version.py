6848eccd56c6a7dcc73792ce45097cca6b199ac8
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-10-19 19:45:20 +0200

    Bump version to 20.7.2

--- pydoctor/_version.py
+++ pydoctor/_version.py
@@ -7,5 +7,5 @@ Provides pydoctor version information.
 
 from incremental import Version
 
-__version__ = Version('pydoctor', 20, 7, 2, release_candidate=2)
+__version__ = Version('pydoctor', 20, 7, 2)
 __all__ = ["__version__"]
