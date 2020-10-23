908c8bd10b9b3231c1706f9f2089f69e9043c7b3
Thomas Grainger <tagrain@gmail.com>
2020-10-23 09:39:03 +0100

    apply setup-py-upgrade

--- pydoctor/__init__.py
+++ pydoctor/__init__.py
@@ -1,5 +1,9 @@
 """PyDoctor, an API documentation generator for Python libraries."""
 
+__all__ = ["__version__"]
+
 from ._version import __version__
 
-__all__ = ["__version__"]
+
+def _setuptools_version():
+    return __version__.public()
