60866ccf82c86d233ffdcace4199eb5e0655668d
Thomas Grainger <tagrain@gmail.com>
2020-10-26 14:13:39 +0000

    type annotate pydoctor._setuptools_version

--- pydoctor/__init__.py
+++ pydoctor/__init__.py
@@ -5,5 +5,5 @@ __all__ = ["__version__"]
 from ._version import __version__
 
 
-def _setuptools_version():
-    return __version__.public()
+def _setuptools_version() -> str:
+    return __version__.public() # type: ignore[no-any-return]
