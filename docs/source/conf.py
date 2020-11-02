c25bf77d58fc6d7b65a09802eb9bd6be3348d633
tristanlatr <tris.la.tr@gmail.com>
2020-11-02 16:52:36 -0500

    Add intersphinx_mapping magic

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Configure intersphinx magic
+intersphinx_mapping = {
+    'pydoctor': ('https://mfesiem.github.io/pydoctor/apidocs/', None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 
