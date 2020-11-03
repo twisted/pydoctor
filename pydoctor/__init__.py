d4b7665e802a56387e7994e3b89458c492fc1644
tristanlatr <tris.la.tr@gmail.com>
2020-11-02 23:28:36 -0500

    Add pydoctor API warning

--- pydoctor/__init__.py
+++ pydoctor/__init__.py
@@ -1,4 +1,13 @@
-"""PyDoctor, an API documentation generator for Python libraries."""
+"""PyDoctor, an API documentation generator for Python libraries.
+
+Warning:
+    PyDoctor's own API isn't stable YET, custom builds are prone to break!
+
+`Return to main documentation <https://mfesiem.github.io/pydoctor/index.html>`_
+
+"""
+
+__docformat__ = "restructuredtext en"
 
 __all__ = ["__version__"]
 
