327cc2ed3c97fa9b04e093688640e20ca6b6659e
Adi Roiban <adi.roiban@chevah.com>
2020-11-05 00:35:20 +0000

    Try serial write.

--- pydoctor/sphinx_ext/api_output.py
+++ pydoctor/sphinx_ext/api_output.py
@@ -49,5 +49,5 @@ def setup(app):
     return {
             'version': '0.1',
             'parallel_read_safe': True,
-            'parallel_write_safe': True,
+            'parallel_write_safe': False,
         }
