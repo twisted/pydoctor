3e4ce2c46facd34ffd462cad81250adbc69a44d3
tristanlatr <tris.la.tr@gmail.com>
2020-11-26 19:45:58 -0500

    Revert "Accept multi_pydoctor_args in conf.py for the build_apidocs sphinx extention"
    This reverts commit 3f5d13c4ee62322af09b28b2f880c28fbc1cf2fd.

--- pydoctor/sphinx_ext/build_apidocs.py
+++ pydoctor/sphinx_ext/build_apidocs.py
@@ -35,34 +35,26 @@ def on_build_finished(app: Sphinx, exception: Exception) -> None:
     Called when Sphinx build is done.
     """
     config: Config = app.config  # type: ignore[has-type]
-    if not config.pydoctor_args and not config.multi_pydoctor_args:
-        raise ConfigError("Missing 'pydoctor_args' (or 'multi_pydoctor_args').")
-    
-    pydoctor_args_list=[]
-    if config.pydoctor_args:
-        pydoctor_args_list.append(config.pydoctor_args)
-    if config.multi_pydoctor_args:
-        pydoctor_args_list.extend(config.multi_pydoctor_args)
+    if not config.pydoctor_args:
+        raise ConfigError("Missing 'pydoctor_args'.")
 
-    for i, pydoctor_args in enumerate(pydoctor_args_list):
+    placeholders = {
+        'outdir': app.outdir,
+        }
 
-        placeholders = {
-            'outdir': app.outdir,
-            }
+    args = []
+    for argument in config.pydoctor_args:
+        args.append(argument.format(**placeholders))
 
-        args = []
-        for argument in pydoctor_args:
-            args.append(argument.format(**placeholders))
+    logger.info("Bulding pydoctor API docs as:")
+    logger.info('\n'.join(args))
 
-        logger.info(f"Bulding pydoctor API docs ({i}) as:")
-        logger.info('\n'.join(args))
+    with StringIO() as stream:
+        with redirect_stdout(stream):
+            main(args=args)
 
-        with StringIO() as stream:
-            with redirect_stdout(stream):
-                main(args=args)
-
-            for line in stream.getvalue().splitlines():
-                logger.warning(line)
+        for line in stream.getvalue().splitlines():
+            logger.warning(line)
 
 
 def setup(app: Sphinx) ->  Dict[str, Any]:
@@ -73,7 +65,6 @@ def setup(app: Sphinx) ->  Dict[str, Any]:
     """
     app.connect('build-finished', on_build_finished)
     app.add_config_value("pydoctor_args", [], "env")
-    app.add_config_value("multi_pydoctor_args", [], "env")
 
     return {
         'version': str(__version__),
