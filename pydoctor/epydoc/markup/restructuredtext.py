eadc55909c7fce947a05d41996bb9027aa45633a
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-12-15 16:28:13 +0100

    Add type annotations to restructuredtext module
    There are no annotations in docutils, so I improvised. This will
    probably need updating if docutils ever gets anntations.
    
    For example the visit methods all take a Node as an argument now,
    while it is likely guaranteed that they're passed a particular
    Node subclass.

--- pydoctor/epydoc/markup/restructuredtext.py
+++ pydoctor/epydoc/markup/restructuredtext.py
@@ -42,8 +42,8 @@ the list.
 """
 __docformat__ = 'epytext en'
 
-# Imports
-from typing import List, Sequence
+from typing import Any, ClassVar, Iterable, List, Optional, Sequence, Set
+import optparse
 import re
 
 from docutils.core import publish_string
@@ -51,9 +51,10 @@ from docutils.writers import Writer
 from docutils.writers.html4css1 import HTMLTranslator, Writer as HTMLWriter
 from docutils.readers.standalone import Reader as StandaloneReader
 from docutils.utils import new_document
-from docutils.nodes import NodeVisitor, SkipNode, Text
+from docutils.nodes import Node, NodeVisitor, SkipNode, Text
 from docutils.frontend import OptionParser
 from docutils.parsers.rst import Directive, directives
+from docutils.transforms import Transform
 import docutils.nodes
 import docutils.transforms.frontmatter
 import docutils.utils
@@ -112,8 +113,11 @@ def parse_docstring(docstring: str, errors: List[ParseError]) -> ParsedDocstring
 class OptimizedReporter(docutils.utils.Reporter):
     """A reporter that ignores all debug messages.  This is used to
     shave a couple seconds off of epydoc's run time, since docutils
-    isn't very fast about processing its own debug messages."""
-    def debug(self, *args, **kwargs): pass
+    isn't very fast about processing its own debug messages.
+    """
+
+    def debug(self, *args: object, **kwargs: object) -> None:
+        pass
 
 class ParsedRstDocstring(ParsedDocstring):
     """
@@ -153,28 +157,32 @@ class _EpydocReader(StandaloneReader):
     and appends them to a list.
     """
 
-    def __init__(self, errors):
+    def __init__(self, errors: List[ParseError]):
         self._errors = errors
         StandaloneReader.__init__(self)
 
-    def get_transforms(self):
+    def get_transforms(self) -> List[Transform]:
         # Remove the DocInfo transform, to ensure that :author: fields
         # are correctly handled.
         return [t for t in StandaloneReader.get_transforms(self)
                 if t != docutils.transforms.frontmatter.DocInfo]
 
-    def new_document(self):
+    def new_document(self) -> docutils.nodes.document:
         document = new_document(self.source.source_path, self.settings)
         # Capture all warning messages.
         document.reporter.attach_observer(self.report)
         # Return the new document.
         return document
 
-    def report(self, error):
-        try: is_fatal = int(error['level']) > 2
-        except: is_fatal = True
-        try: linenum = int(error['line'])
-        except: linenum = None
+    def report(self, error: docutils.nodes.system_message) -> None:
+        try:
+            is_fatal = int(error['level']) > 2
+        except:
+            is_fatal = True
+        try:
+            linenum: Optional[int] = int(error['line'])
+        except:
+            linenum = None
 
         msg = ''.join(c.astext() for c in error)
 
@@ -186,16 +194,13 @@ class _DocumentPseudoWriter(Writer):
     access the document itself.  The output of C{_DocumentPseudoWriter}
     is just an empty string; but after it has been used, the most
     recently processed document is available as the instance variable
-    C{document}
-
-    @type document: C{docutils.nodes.document}
-    @ivar document: The most recently processed document.
+    C{document}.
     """
-    def __init__(self):
-        self.document = None
-        Writer.__init__(self)
 
-    def translate(self):
+    document: docutils.nodes.document
+    """The most recently processed document."""
+
+    def translate(self) -> None:
         self.output = ''
 
 class _SplitFieldsTranslator(NodeVisitor):
@@ -214,16 +219,16 @@ class _SplitFieldsTranslator(NodeVisitor):
     consolidated fields expressed as unordered lists still require
     backticks for now."""
 
-    def __init__(self, document, errors):
+    def __init__(self, document: docutils.nodes.document, errors: List[ParseError]):
         NodeVisitor.__init__(self, document)
         self._errors = errors
-        self.fields = []
-        self._newfields = set()
+        self.fields: List[Field] = []
+        self._newfields: Set[str] = set()
 
-    def visit_document(self, node):
+    def visit_document(self, node: Node) -> None:
         self.fields = []
 
-    def visit_field(self, node):
+    def visit_field(self, node: Node) -> None:
         # Remove the field from the tree.
         node.parent.remove(node)
 
@@ -257,18 +262,23 @@ class _SplitFieldsTranslator(NodeVisitor):
 
         self._add_field(tagname, arg, fbody, node.line)
 
-    def _add_field(self, tagname, arg, fbody, lineno):
+    def _add_field(self,
+            tagname: str,
+            arg: Optional[str],
+            fbody: Iterable[Node],
+            lineno: int
+            ) -> None:
         field_doc = self.document.copy()
         for child in fbody: field_doc.append(child)
         field_pdoc = ParsedRstDocstring(field_doc, ())
         self.fields.append(Field(tagname, arg, field_pdoc, lineno - 1))
 
-    def visit_field_list(self, node):
+    def visit_field_list(self, node: Node) -> None:
         # Remove the field list from the tree.  The visitor will still walk
         # over the node's children.
         node.parent.remove(node)
 
-    def handle_consolidated_field(self, body, tagname):
+    def handle_consolidated_field(self, body: Sequence[Node], tagname: str) -> None:
         """
         Attempt to handle a consolidated section.
         """
@@ -285,7 +295,7 @@ class _SplitFieldsTranslator(NodeVisitor):
         else:
             raise ValueError('does not contain a bulleted list.')
 
-    def handle_consolidated_bullet_list(self, items, tagname):
+    def handle_consolidated_bullet_list(self, items: Iterable[Node], tagname: str) -> None:
         # Check the contents of the list.  In particular, each list
         # item should have the form:
         #   - `arg`: description...
@@ -336,7 +346,7 @@ class _SplitFieldsTranslator(NodeVisitor):
             # Wrap the field body, and add a new field
             self._add_field(tagname, arg, fbody, fbody[0].line)
 
-    def handle_consolidated_definition_list(self, items, tagname):
+    def handle_consolidated_definition_list(self, items: Iterable[Node], tagname: str) -> None:
         # Check the list contents.
         n = 0
         _BAD_ITEM = ("item %d is not well formed.  Each item's term must "
@@ -370,14 +380,19 @@ class _SplitFieldsTranslator(NodeVisitor):
                 type_descr = item[1]
                 self._add_field('type', arg, type_descr, lineno)
 
-    def unknown_visit(self, node):
+    def unknown_visit(self, node: Node) -> None:
         'Ignore all unknown nodes'
 
 _TARGET_RE = re.compile(r'^(.*?)\s*<(?:URI:|URL:)?([^<>]+)>$')
 
 class _EpydocHTMLTranslator(HTMLTranslator):
-    settings = None
-    def __init__(self, document, docstring_linker):
+
+    settings: ClassVar[Optional[optparse.Values]] = None
+
+    def __init__(self,
+            document: docutils.nodes.document,
+            docstring_linker: DocstringLinker
+            ):
         self._linker = docstring_linker
 
         # Set the document's settings.
@@ -389,7 +404,7 @@ class _EpydocHTMLTranslator(HTMLTranslator):
         super().__init__(document)
 
     # Handle interpreted text (crossreferences)
-    def visit_title_reference(self, node):
+    def visit_title_reference(self, node: Node) -> None:
         m = _TARGET_RE.match(node.astext())
         if m: text, target = m.groups()
         else: target = text = node.astext()
@@ -406,16 +421,19 @@ class _EpydocHTMLTranslator(HTMLTranslator):
         self.body.append(flatten(xref))
         raise SkipNode()
 
-    def should_be_compact_paragraph(self, node):
+    def should_be_compact_paragraph(self, node: Node) -> bool:
         if self.document.children == [node]:
             return True
         else:
-            return super().should_be_compact_paragraph(node)
+            return super().should_be_compact_paragraph(node)  # type: ignore[no-any-return]
 
-    def visit_document(self, node): pass
-    def depart_document(self, node): pass
+    def visit_document(self, node: Node) -> None:
+        pass
 
-    def starttag(self, node, tagname, suffix='\n', **attributes):
+    def depart_document(self, node: Node) -> None:
+        pass
+
+    def starttag(self, node: Node, tagname: str, suffix: str = '\n', **attributes: Any) -> str:
         """
         This modified version of starttag makes a few changes to HTML
         tags, to prevent them from conflicting with epydoc.  In particular:
@@ -455,9 +473,9 @@ class _EpydocHTMLTranslator(HTMLTranslator):
             attributes['class'] = ' '.join([attributes.get('class',''),
                                             'heading']).strip()
 
-        return super().starttag(node, tagname, suffix, **attributes)
+        return super().starttag(node, tagname, suffix, **attributes)  # type: ignore[no-any-return]
 
-    def visit_doctest_block(self, node):
+    def visit_doctest_block(self, node: Node) -> None:
         pysrc = node[0].astext()
         if node.get('codeblock'):
             self.body.append(flatten(colorize_codeblock(pysrc)))
@@ -478,7 +496,7 @@ class PythonCodeDirective(Directive):
 
     has_content = True
 
-    def run(self):
+    def run(self) -> List[Node]:
         text = '\n'.join(self.content)
         node = docutils.nodes.doctest_block(text, text, codeblock=True)
         return [ node ]
