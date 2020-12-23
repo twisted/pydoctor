2926c4f14b23cdcaa10ece362f4dad3aacc9ea72
Adi Roiban <adi.roiban@chevah.com>
2020-12-23 20:36:30 +0000

    Update docs.

--- pydoctor/sphinx_ext/build_apidocs.py
+++ pydoctor/sphinx_ext/build_apidocs.py
@@ -10,8 +10,7 @@ Inside the Sphinx conf.py file you need to define the following configuration op
                            You can use C{{rtd_version}} to have the URL automatically updated
                            based on Read The Docs build.
 
-  - C{pydoctor_args} - an sequence with all the pydoctor command line arguments used to trigger the build.
-                       Don't include the C{--make-html} argument, as it will be added automatically.
+  - C{pydoctor_args} - Sequence with all the pydoctor command line arguments used to trigger the build.
                      - (private usage) a mapping with values as sequence of pydoctor command line arguments.
 
 The following format placeholders are resolved for C{pydoctor_args} at runtime:
