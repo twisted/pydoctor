4de6c24c2dce43cb58b51d7bdc9de066d06634d8
Adi Roiban <adi.roiban@chevah.com>
2020-12-28 19:56:54 +0000

    Use a mapping for pydoctor_url_path for multi project builds.

--- pydoctor/sphinx_ext/build_apidocs.py
+++ pydoctor/sphinx_ext/build_apidocs.py
@@ -9,6 +9,8 @@ Inside the Sphinx conf.py file you need to define the following configuration op
   - C{pydoctor_url_path} - defined the URL path to the API documentation
                            You can use C{{rtd_version}} to have the URL automatically updated
                            based on Read The Docs build.
+                        - (private usage) a mapping with values URL path definition.
+                          Make sure each definition will produce a unique URL.
 
   - C{pydoctor_args} - Sequence with all the pydoctor command line arguments used to trigger the build.
                      - (private usage) a mapping with values as sequence of pydoctor command line arguments.
@@ -86,6 +88,10 @@ def on_config_inited(app: Sphinx, config: Config) -> None:
         # We have a single pydoctor call
         runs = {'main': runs}
 
+    pydoctor_url_path = config.pydoctor_url_path
+    if not isinstance(runs, Mapping):
+        pydoctor_url_path = {'main': pydoctor_url_path}
+
     for key, value in runs.items():
         arguments = _get_arguments(value, placeholders)
 
@@ -94,10 +100,10 @@ def on_config_inited(app: Sphinx, config: Config) -> None:
         temp_path = output_path.with_suffix('.pydoctor_temp')
 
         # Update intersphinx_mapping.
-        pydoctor_url_path = config.pydoctor_url_path
-        if pydoctor_url_path:
+        url_path = pydoctor_url_path.get(key)
+        if url_path:
             intersphinx_mapping = config.intersphinx_mapping
-            url = pydoctor_url_path.format(**{'rtd_version': rtd_version})
+            url = url_path.format(**{'rtd_version': rtd_version})
             intersphinx_mapping[key + '-api-docs'] = (url, str(temp_path / 'objects.inv'))
 
         # Build the API docs in temporary path.
@@ -146,7 +152,7 @@ def setup(app: Sphinx) ->  Mapping[str, Any]:
     @return: The extension version and runtime options.
     """
     app.add_config_value("pydoctor_args", None, "env")
-    app.add_config_value("pydoctor_url_path", "", "env")
+    app.add_config_value("pydoctor_url_path", None, "env")
 
     # Make sure we have a lower priority than intersphinx extension.
     app.connect('config-inited', on_config_inited, priority=790)
