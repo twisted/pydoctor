c46a348da113580e6470f0b993bc66f8be64863b
Maarten ter Huurne <maarten@boxingbeetle.com>
2021-01-08 13:27:14 +0100

    Use Sphinx spell check extension
    I had to re-add the "build everything" (`-aE`) option because the
    spell checker breaks on smart quotes. The extension does disable
    smart quotes during the `spelling` build, but if it re-uses cached
    files from the `html` build, those still contain smart quotes.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,6 +37,7 @@ extensions = [
     "sphinx.ext.intersphinx",
     "pydoctor.sphinx_ext._help_output",
     "pydoctor.sphinx_ext.build_apidocs",
+    "sphinxcontrib.spelling",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -46,6 +47,9 @@ templates_path = ['_templates']
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
+
+# Configure spell checker.
+spelling_word_list_filename = 'spelling_wordlist.txt'
 
 # Configure intersphinx magic
 intersphinx_mapping = {
