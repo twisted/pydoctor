69e3e76f15b8f5f4de7fded932f01764743fd25f
Maarten ter Huurne <maarten@boxingbeetle.com>
2021-01-23 02:33:03 +0100

    Add query string parameter for showing private API
    Links and bookmarks can now opt to show private API on page load.
    
    If a browser does not support searchParams, we fall back to the
    current behavior of hiding private API on page load.
    
    If a browser does not support the History API, we don't update the
    location bar, but the toggle still works.

--- pydoctor/templates/pydoctor.js
+++ pydoctor/templates/pydoctor.js
@@ -1,13 +1,21 @@
+function initPrivate() {
+    var params = (new URL(document.location)).searchParams;
+    if (!params || !parseInt(params.get('private'))) {
+        document.body.classList.add("private-hidden");
+    }
+    updatePrivate();
+}
 function togglePrivate() {
-    // Hide all private things by adding the private-hidden class to them.
     document.body.classList.toggle("private-hidden");
-
-    var button = document.querySelector('#showPrivate button');
-    if(document.body.classList.contains('private-hidden')) {
-        button.innerText = 'Show Private API';
-    } else {
-        button.innerText = 'Hide Private API';
+    updatePrivate();
+}
+function updatePrivate() {
+    var hidden = document.body.classList.contains('private-hidden');
+    document.querySelector('#showPrivate button').innerText =
+        hidden ? 'Show Private API' : 'Hide Private API';
+    if (history) {
+        var search = hidden ? document.location.pathname : '?private=1';
+        history.replaceState(null, '', search + document.location.hash);
     }
 }
-// On load, hide everything private
-togglePrivate()
+initPrivate();
