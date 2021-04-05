7b8b10272b11c7d7852d5e96bb0d3d979ac40325
tristanlatr <tris.la.tr@gmail.com>
2021-04-05 17:03:51 -0400

    Add warning and private infos. Change a few other minor things to make it pretty

--- pydoctor/templates/search.js
+++ pydoctor/templates/search.js
@@ -12,12 +12,26 @@ function setInfos(message) {
   }
 }
 
+function setPrivateInfos(message) {
+  document.getElementById('search-private-infos').textContent = message;
+}
+
+function setWarning(message) {
+  document.getElementById('search-warn').textContent = message;
+  if (message.length>0){
+    document.getElementById('search-warn-box').style.display = 'block'
+  }
+  else{
+    document.getElementById('search-warn-box').style.display = 'none'
+  }
+}
+
 function setErrorStatus() {
-  setStatus("Something went wrong. See development console for details.");
+  setStatus("Something went wrong.");
 }
 
 function setErrorInfos(message) {
-  setInfos("Error: " +  message);
+  setWarning("Error: " +  message + ". See development console for details.");
 }
 
 function buildSearchResult(dobj) {
@@ -58,10 +72,36 @@ function buildSearchResult(dobj) {
 }
 
 function setLongSearchInfos(){
-  setInfos("This is taking longer than usual... You can keep waiting for the search to complete, or retry the search with other terms.")
+  setWarning("This is taking longer than usual... You can keep waiting for the search to complete, or retry the search with other terms.")
+}
+
+
+function buildInfosString(search_results_documents, priv){
+
+  let nb_classes = search_results_documents.filter(function(value){
+    return (value.querySelector('.type').innerHTML.endsWith("Class") && (value.querySelector('.privacy').innerHTML.endsWith(priv ? "PRIVATE" : "VISIBLE")))
+  }).length;  
+
+  let nb_methods_or_functions = search_results_documents.filter(function(value){
+    return (value.querySelector('.type').innerHTML.endsWith("Function") && (value.querySelector('.privacy').innerHTML.endsWith(priv ? "PRIVATE" : "VISIBLE")))
+  }).length;  
+
+  let nb_module_or_package = search_results_documents.filter(function(value){
+    return (value.querySelector('.type').innerHTML.endsWith("Module") || value.querySelector('.kind').innerHTML.endsWith("Package")) && (value.querySelector('.privacy').innerHTML.endsWith(priv ? "PRIVATE" : "VISIBLE"))
+  }).length;  
+
+  let nb_var = search_results_documents.filter(function(value){
+    return (value.querySelector('.type').innerHTML.endsWith("Attribute")) && (value.querySelector('.privacy').innerHTML.endsWith(priv ? "PRIVATE" : "VISIBLE"))
+
+  }).length; 
+
+  return('Including ' + nb_classes.toString() + (priv ? " private" : "") + ' class(es), ' + nb_methods_or_functions.toString() + (priv ? " private" : "") + 
+    ' method(s) and/or function(s), ' + nb_module_or_package + (priv ? " private" : "") + ' module(s) and/or package(s) and ' + nb_var + (priv ? " private" : "") + ' attribute(s).')
 }
 
 ///////////////////// MAIN JS ROUTINE //////////////////////////
+
+function search(){
 
 let _url = new URL(document.URL);
 console.log(_url);
@@ -108,17 +148,19 @@ else{
           clearTimeout(_setLongSearchInfosTimeout)
         }
         setInfos('')
+        setWarning('')
         
         console.log("Message received from worker: ")
         console.dir(response.data)
 
         if (!response.data.results){
-          return ;
+          return;
           // Error will be reported by onerror
         }
 
         if (response.data.results.length == 0){
           setStatus('No results matches "' + _query + '"');
+          return;
         }
         else{
           setStatus('Fetching documents...');
@@ -146,10 +188,8 @@ else{
 
           });
 
-          var warn = ''
-
-          if (response.data.results[0].score <= 20){
-            warn = " Unfortunately, it looks like there aren't many great matches for your search."
+          if (response.data.results[0].score <= 15){
+            setWarning("Unfortunately, it looks like there aren't many great matches for your search.")
           }
 
           let public_search_results = search_results_documents.filter(function(value){
@@ -161,33 +201,19 @@ else{
             setInfos('Some private objects matches your search though.')
           }
           else{
-            history.replaceState(null, "Search", "?search-query=" + _query)
+
             setStatus(
               'Search for "' + _query + '" yielded ' + public_search_results.length + ' ' +
-              (public_search_results.length === 1 ? 'result' : 'results') + '.' + warn);
+              (public_search_results.length === 1 ? 'result' : 'results') + '.');
 
             // Build complementary information string
 
-            let nb_classes = search_results_documents.filter(function(value){
-              return (value.querySelector('.type').innerHTML.endsWith("Class") && (value.querySelector('.privacy').innerHTML.endsWith("VISIBLE")))
-            }).length;  
-
-            let nb_methods_or_functions = search_results_documents.filter(function(value){
-              return (value.querySelector('.type').innerHTML.endsWith("Function") && (value.querySelector('.privacy').innerHTML.endsWith("VISIBLE")))
-            }).length;  
-
-            let nb_module_or_package = search_results_documents.filter(function(value){
-              return (value.querySelector('.type').innerHTML.endsWith("Module") || value.querySelector('.kind').innerHTML.endsWith("Package")) && (value.querySelector('.privacy').innerHTML.endsWith("VISIBLE"))
-            }).length;  
-
-            let nb_var = search_results_documents.filter(function(value){
-              return (value.querySelector('.type').innerHTML.endsWith("Attribute")) && (value.querySelector('.privacy').innerHTML.endsWith("VISIBLE"))
-
-            }).length; 
-
-            setInfos('Including ' + nb_classes.toString() + ' class(es), ' + nb_methods_or_functions.toString() + 
-              ' method(s) and/or function(s), ' + nb_module_or_package + ' module(s) and/or package(s) and ' + nb_var + ' attribute(s).')
+            setInfos(buildInfosString(search_results_documents, false))
           }
+
+          // Build PRIVATE complementary information string
+
+          setPrivateInfos(buildInfosString(search_results_documents, true))
 
         },
         function(error){
@@ -208,8 +234,11 @@ else{
       }
 
 
-      _setLongSearchInfosTimeout = setTimeout(setLongSearchInfos, 10000)
+      _setLongSearchInfosTimeout = setTimeout(setLongSearchInfos, 8000)
     }
   }
 }
 
+}
+
+search()
