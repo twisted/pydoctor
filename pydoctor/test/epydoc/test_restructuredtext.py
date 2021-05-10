1efe6be182bf11d01dbdadd85e7c2ff46f35edd8
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2021-05-10 13:45:33 -0400

    Add a test for the get_toc() method

--- pydoctor/test/epydoc/test_restructuredtext.py
+++ pydoctor/test/epydoc/test_restructuredtext.py
@@ -143,3 +143,78 @@ def test_rst_directive_deprecated() -> None:
         <p><span class="versionmodified deprecated">Deprecated since version 0.2: For security reasons</span></p>
         </div>"""
     assert prettify(html) == prettify(expected_html)
+
+def test_get_toc() -> None:
+
+    docstring = """
+Titles
+======
+
+Level 2
+-------
+
+Level 3
+~~~~~~~
+
+Level 4
+^^^^^^^
+
+Level 5
+!!!!!!!
+
+Lists
+=====
+
+Other
+=====
+"""
+
+    errors: List[ParseError] = []
+    parsed = parse_docstring(docstring, errors)
+    assert not errors
+    
+    toc = parsed.get_toc(4)
+    assert toc is not None
+    html = flatten(toc.to_stan(NotFoundLinker()))
+    
+    expected_html="""
+     <li>
+  <p class="rst-first">
+   <a class="rst-reference internal" href="#rst-titles" id="rst-id1">
+    Titles
+   </a>
+  </p>
+  <ul class="rst-simple">
+   <li>
+    <a class="rst-reference internal" href="#rst-level-2" id="rst-id2">
+     Level 2
+    </a>
+    <ul>
+     <li>
+      <a class="rst-reference internal" href="#rst-level-3" id="rst-id3">
+       Level 3
+      </a>
+      <ul>
+       <li>
+        <a class="rst-reference internal" href="#rst-level-4" id="rst-id4">
+         Level 4
+        </a>
+       </li>
+      </ul>
+     </li>
+    </ul>
+   </li>
+  </ul>
+ </li>
+ <li>
+  <a class="rst-reference internal" href="#rst-lists" id="rst-id5">
+   Lists
+  </a>
+ </li>
+ <li>
+  <a class="rst-reference internal" href="#rst-other" id="rst-id6">
+   Other
+  </a>
+ </li>
+    """
+    assert prettify(html) == prettify(expected_html)
