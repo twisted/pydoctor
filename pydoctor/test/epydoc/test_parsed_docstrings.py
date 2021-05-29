30e1bbd7a7a3756a3f9cfdc2b903c26f22662f45
tristanlatr <19967168+tristanlatr@users.noreply.github.com>
2021-05-29 14:36:49 -0400

    Fix mypy

--- pydoctor/test/epydoc/test_parsed_docstrings.py
+++ pydoctor/test/epydoc/test_parsed_docstrings.py
@@ -7,6 +7,7 @@ from pydoctor.epydoc.markup import ParsedDocstring, ParseError, flatten
 from pydoctor.epydoc.markup.plaintext import parse_docstring
 from pydoctor.test.epydoc.test_epytext2html import parse_epytext
 from pydoctor.test.epydoc.test_restructuredtext import parse_rst, prettify
+from pydoctor.test import NotFoundLinker
 
 def parse_plaintext(s: str) -> ParsedDocstring:
     errors: List[ParseError] = []
@@ -23,12 +24,12 @@ def test_to_node_to_stan_caching() -> None:
     """
     epy = parse_epytext('Just some B{strings}')
     assert epy.to_node() == epy.to_node() == epy.to_node()
-    assert flatten_(epy.to_stan(None)) == flatten_(epy.to_stan(None)) == flatten_(epy.to_stan(None))
+    assert flatten_(epy.to_stan(NotFoundLinker())) == flatten_(epy.to_stan(NotFoundLinker())) == flatten_(epy.to_stan(NotFoundLinker()))
 
     rst = parse_rst('Just some **strings**')
     assert rst.to_node() == rst.to_node() == rst.to_node()
-    assert flatten_(rst.to_stan(None)) == flatten_(rst.to_stan(None)) == flatten_(rst.to_stan(None))
+    assert flatten_(rst.to_stan(NotFoundLinker())) == flatten_(rst.to_stan(NotFoundLinker())) == flatten_(rst.to_stan(NotFoundLinker()))
 
     plain = parse_plaintext('Just some **strings**')
     assert plain.to_node() == plain.to_node() == plain.to_node()
-    assert flatten_(plain.to_stan(None)) == flatten_(plain.to_stan(None)) == flatten_(plain.to_stan(None))
+    assert flatten_(plain.to_stan(NotFoundLinker())) == flatten_(plain.to_stan(NotFoundLinker())) == flatten_(plain.to_stan(NotFoundLinker()))
