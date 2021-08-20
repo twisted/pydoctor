6037fcfa22f68192f1879c4eb33de79fc6ea144d
tristanlatr <tris.la.tr@gmail.com>
2021-08-20 01:18:19 -0400

    Re-arrange the tests in the docs folders into a package.
    Create a failling test for https://twistedmatrix.com/trac/ticket/10197.

---
+++ docs/tests/test_twisted_docs.py
@@ -0,0 +1,27 @@
+#
+# Run tests after Twisted's the documentation is executed.
+#
+# These tests are designed to be executed inside tox, after bin/admin/build-apidocs.
+#
+
+import pathlib
+import os
+import pytest
+
+BASE_DIR = pathlib.Path(os.environ.get('TOX_WORK_DIR', os.getcwd())) / 'twisted-apidocs-build'
+
+# Failling test for https://twistedmatrix.com/trac/ticket/10197
+@pytest.mark.xfail
+def test_IPAddress_implementations() -> None:
+    """
+    There is a flaw in the logic, currently.
+    """
+
+    implementations_that_currently_do_not_show_up = ['twisted.internet.address.IPv4Address', 
+        'twisted.internet.address.IPv6Address', 
+        'twisted.internet.address.HostnameAddress', 
+        'twisted.internet.address.UNIXAddress']
+
+    with open(BASE_DIR / 'twisted.internet.interfaces.IAddress.html') as stream:
+        page = stream.read()
+        assert all(impl in page for impl in implementations_that_currently_do_not_show_up), page
