da017e8c89a73a874731284f538911f1be60278a
tristanlatr <tris.la.tr@gmail.com>
2021-10-05 11:26:38 -0400

    remove unused import.

--- pydoctor/templatewriter/__init__.py
+++ pydoctor/templatewriter/__init__.py
@@ -1,5 +1,5 @@
 """Render pydoctor data as HTML."""
-from typing import Iterable, Iterator, Optional, Union, cast, overload, TYPE_CHECKING
+from typing import Iterable, Iterator, Optional, Union, cast, TYPE_CHECKING
 if TYPE_CHECKING:
     from typing_extensions import Protocol, runtime_checkable
 else:
