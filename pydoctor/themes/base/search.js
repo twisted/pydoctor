3dc6e4ee923d88eecc5c126bed7dc78908371180
tristanlatr <trislatr@gmail.com>
2022-02-21 02:16:17 -0500

    re search on enter

--- pydoctor/themes/base/search.js
+++ pydoctor/themes/base/search.js
@@ -126,13 +126,7 @@ window.addEventListener("click", function(event) {
 // Close the dropdown if the use click on echap key
 document.onkeydown = function(evt) {
   evt = evt || window.event;
-  var isEscape = false;
-  if ("key" in evt) {
-      isEscape = (evt.key === "Escape" || evt.key === "Esc");
-  } else {
-      isEscape = (evt.keyCode === 27);
-  }
-  if (isEscape) {
+  if (evt.key === "Escape" || evt.key === "Esc") {
       results_container.style.display = 'none';
   }
 };
@@ -179,6 +173,7 @@ function search(){
 
   if (!_query.length>0){
     results_list.innerHTML = '';
+    setStatus('')
     return ;
   }
 
@@ -294,7 +289,7 @@ function search(){
   _setLongSearchInfosTimeout = setTimeout(setLongSearchInfos, 8000);
 }
 
-search_function = function(event){
+function launch_function(){
   try{
     worker.terminate()
     if (_setLongSearchInfosTimeout){
@@ -311,4 +306,11 @@ search_function = function(event){
   }
 };
 
-input.addEventListener('input', search_function);
+input.addEventListener('input',function(event) {
+  launch_function();
+});
+input.addEventListener("keyup", function(event) {
+  if (event.key === 'Enter') {
+    launch_function();
+  }
+});
