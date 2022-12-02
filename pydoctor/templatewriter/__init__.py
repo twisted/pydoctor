d4698a50ca7c4fbb61c6b824367bb87307400969
tristanlatr <tristanlatr@users.noreply.github.com>
2022-12-01 21:25:25 -0500

    No, actually python 3.11, because we have some name resolving that uses latest standard linrary object names that would otherwise would create a target not found error.

--- pydoctor/templatewriter/__init__.py
+++ pydoctor/templatewriter/__init__.py
@@ -14,8 +14,10 @@ from xml.dom import minidom
 
 # Newer APIs from importlib_resources should arrive to stdlib importlib.resources in Python 3.9.
 if TYPE_CHECKING:
-    if sys.version_info >= (3, 9):
+    if sys.version_info >= (3,11):
         from importlib.resources.abc import Traversable
+    elif sys.version_info >= (3,9):
+        from importlib.abc import Traversable
     else:
         Traversable = Any
 else:
