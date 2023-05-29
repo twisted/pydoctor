4e3e6fa61329be467a7cedf796958f68250c4a07
tristanlatr <tristanlatr@users.noreply.github.com>
2023-05-29 09:22:49 -0400

    Fix mypy

--- pydoctor/astbuilder.py
+++ pydoctor/astbuilder.py
@@ -88,14 +88,11 @@ class TypeAliasVisitorExt(extensions.ModuleVisitorExt):
         Return C{True} if the Attribute is a type alias.
         """
         if ob.value is not None:
-
             if is_using_annotations(ob.annotation, ('typing.TypeAlias', 
                                                     'typing_extensions.TypeAlias'), ob):
                 return True
-            
             if is_typing_annotation(ob.value, ob.parent):
                 return True
-        
         return False
 
     def visit_Assign(self, node: Union[ast.Assign, ast.AnnAssign]) -> None:
@@ -110,7 +107,9 @@ class TypeAliasVisitorExt(extensions.ModuleVisitorExt):
                 if self._isTypeAlias(attr) is True:
                     attr.kind = model.DocumentableKind.TYPE_ALIAS
                     # unstring type aliases
-                    attr.value = unstring_annotation(attr.value, attr, section='type alias')
+                    attr.value = unstring_annotation(
+                        # this cast() is safe because _isTypeAlias() return True only if value is not None
+                        cast(attr.value, ast.expr), attr, section='type alias')
                 elif self._isTypeVariable(attr) is True:
                     # TODO: unstring bound argument of type variables
                     attr.kind = model.DocumentableKind.TYPE_VARIABLE
