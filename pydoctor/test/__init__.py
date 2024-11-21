1180dc9eb31d63eb55845193d00c031fcf7e894d
tristanlatr <tristanlatr@users.noreply.github.com>
2024-11-21 12:16:36 -0500

    Fix pyflakes

--- pydoctor/test/__init__.py
+++ pydoctor/test/__init__.py
@@ -1,20 +1,13 @@
 """PyDoctor's test suite."""
 
-import contextlib
 from logging import LogRecord
-from typing import Iterable, TYPE_CHECKING, Iterator, Optional, Sequence
+from typing import Iterable, TYPE_CHECKING, Sequence
 import sys
 import pytest
 from pathlib import Path
 
-from twisted.web.template import Tag, tags
-
 from pydoctor import epydoc2stan, model
 from pydoctor.templatewriter import IWriter, TemplateLookup
-from pydoctor.linker import NotFoundLinker
-
-if TYPE_CHECKING:
-    from twisted.web.template import Flattenable
 
 posonlyargs = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8")
 typecomment = pytest.mark.skipif(sys.version_info < (3, 8), reason="requires python 3.8")
