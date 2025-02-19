7eb22b7db31a10a1f4a89f40a6333f21aedfb5f5
tristanlatr <tristanlatr@users.noreply.github.com>
2025-02-19 18:55:40 -0500

    Remove unused import

--- pydoctor/epydoc/markup/_types.py
+++ pydoctor/epydoc/markup/_types.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 
 from typing import Callable, Dict, List, Union
 
-from pydoctor.epydoc.markup import ParseError, ParsedDocstring
+from pydoctor.epydoc.markup import ParsedDocstring
 from pydoctor.epydoc.markup._pyval_repr import PyvalColorizer
 from pydoctor.napoleon.docstring import TokenType, TypeDocstring
 from pydoctor.epydoc.docutils import new_document, set_node_attributes
