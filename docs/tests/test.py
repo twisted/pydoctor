4bb1065b7bf593b3810598bb18b35fe34ab0823f
tristanlatr <tristanlatr@users.noreply.github.com>
2025-02-28 09:24:01 -0500

    Adjust test

--- docs/tests/test.py
+++ docs/tests/test.py
@@ -197,8 +197,6 @@ def test_lunr_index() -> None:
                     'pydoctor.epydoc.markup.plaintext.ParsedPlaintextDocstring.to_stan',
                     'pydoctor.epydoc.markup._types.ParsedTypeDocstring.to_stan',
                     'pydoctor.epydoc.markup._pyval_repr.ColorizedPyvalRepr.to_stan',
-                    'pydoctor.epydoc.markup._ParsedDocstringTree.to_stan',
-                    'pydoctor.epydoc.markup._ParsedDocstringWithTag.to_stan',
                 ]
         test_search('to_stan*', to_stan_results, order_is_important=False)
         test_search('to_stan', to_stan_results, order_is_important=False)
@@ -209,8 +207,6 @@ def test_lunr_index() -> None:
                     'pydoctor.epydoc.markup._types.ParsedTypeDocstring.to_node',
                     'pydoctor.epydoc.markup.restructuredtext.ParsedRstDocstring.to_node',
                     'pydoctor.epydoc.markup.epytext.ParsedEpytextDocstring.to_node',
-                    'pydoctor.epydoc.markup._ParsedDocstringTree.to_node',
-                    'pydoctor.epydoc.markup._ParsedDocstringWithTag.to_node',
                 ]
         test_search('to_node*', to_node_results, order_is_important=False)
         test_search('to_node', to_node_results, order_is_important=False)
